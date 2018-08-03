6e73c1a6fc47c5ff605768fae092e9e43c3399e0
Matthew Roosa <mroosa@agency-m.com>
2018-08-03 11:25:56 -0400

    wide content

--- design/expressive-x.php
+++ design/expressive-x.php
@@ -21,7 +21,7 @@
           <h1>Expressive X</h1>
           <section class="section" id="top">
             <h2 class="section-title">Section</h2>
-            <p>Placeholder. Need content</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <a class="button" href="#">Download Something</a>
           </section>
 

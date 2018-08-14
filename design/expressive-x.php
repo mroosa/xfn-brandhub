34d0e1d2c38931b24d02f880d2a24d34895e670c
Matthew Roosa <mroosa@agency-m.com>
2018-08-14 12:06:09 -0400

    Updated gallery with text, mobile friendly sub nav

--- design/expressive-x.php
+++ design/expressive-x.php
@@ -60,66 +60,88 @@
 
           <section class="section">
             <h2 class="section-title">Rules of the X</h2>
-            <h3>Completed Expressive X</h3>
-            <p>The final expressive X art will bring together all important elements - subject, background and X guidelines. Go through the carousel to see the rules of the X.</p>
             <div class="gallery">
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-00.jpeg" alt="" />
+                <p class="caption"><strong>Completed Expressive X</strong><br />The final expressive X art will bring together all important elements - subject, background and X guidelines. Go through the carousel to see the rules of the X.</p>
               </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-01.jpeg" alt="" />
+                <p class="caption"><strong>Subject</strong><br />For the most part, these sponsorship/partnerships will promote featured artists, athletes, actors or characters. The first thing to do is to examine the provided artwork to determine where the energy of the subject is directed.</p>
               </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-02.jpeg" alt="" />
+                <p class="caption"><strong>Construction</strong><br />The X should always be low contrast in relation to the background – it shouldn’t be too loud as to overpower what we’re promoting. There are 3 corners from which the foreground art can emerge – left, right, and bottom. The energy of the subject on the previous page will determine which corner of the X the subject will break out of.</p>
               </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-03.jpeg" alt="" />
+                <p class="caption"><strong>Tone</strong><br />Tonally, the X should look like a rip in the background, not an object on top of it. To achieve this, use a darker X than the background on a dark background, and a lighter X than the background on a light background.</p>
               </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-04.jpeg" alt="" />
+                <p class="caption"><strong>Crop</strong><br />The X tends to be more successful when it is cropped, conveying the idea of endlessness – endless content, infinite experiences. The X should be cropped just above the curve of the tail – no shorter and no longer.</p>
               </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-05.jpeg" alt="" />
+                <p class="caption"><strong>Float</strong><br />If the X works better with the art floating, follow the illustration below for spacing.</p>
               </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-06.jpeg" alt="" />
+                <p class="caption"><strong>Emerging Right</strong><br />In this execution the subject’s energy is moving right, so he steps out from behind the left corner of the X.</p>
               </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-07.jpeg" alt="" />
+                <p class="caption"><strong>Emerging Bottom</strong><br />In this execution the subject’s energy is centered and moving up, so he leaps out of the bottom corner of the X.</p>
               </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-08.jpeg" alt="" />
+                <p class="caption"><strong>Emerging Left</strong><br />In this execution the subject’s energy is moving left, so he steps out from behind the right corner of the X.</p>
               </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-rules-3-09.jpeg" alt="" />
+                <p class="caption"><strong>2 Corners</strong><br />It is also possible to tuck the subject behind 2 corners of the X if more space is needed around the artwork or if tells a more interesting story.</p>
+              </div>
+              <div class="img img-w-caption">
+                <img src="<?php print $dir; ?>images/design-rules-3-00.jpeg" alt="" />
+                <p class="caption"><strong>Background Element</strong><br />The final element to creating expressive X art is to add a background. Most properties will have a background – behind a movie star, album art, video game packaging etc.. Once the X and foreground subject have been crafted, the background element should be placed behind and integrated.</p>
               </div>
             </div>
           </section>
 
           <section class="section">
             <h2 class="section-title">Building the X</h2>
-            <p>Here's a simple list of steps to help you create consistent expressive X artwork.</p>
             <div class="gallery">
-              <div class="img">
-                <img src="<?php print $dir; ?>images/design-building-4-01.jpg" alt="" />
-              </div>
-              <div class="img">
-                <img src="<?php print $dir; ?>images/design-building-4-02.jpg" alt="" />
-              </div>
-              <div class="img">
-                <img src="<?php print $dir; ?>images/design-building-4-03.jpg" alt="" />
-              </div>
-              <div class="img">
-                <img src="<?php print $dir; ?>images/design-building-4-04.jpg" alt="" />
-              </div>
-              <div class="img">
-                <img src="<?php print $dir; ?>images/design-building-4-05.jpg" alt="" />
-              </div>
-              <div class="img">
-                <img src="<?php print $dir; ?>images/design-building-4-06.jpg" alt="" />
-              </div>
-              <div class="img">
+              <div class="img img-w-caption">
                 <img src="<?php print $dir; ?>images/design-building-4-07.jpg" alt="" />
+                <p class="caption">Here's a simple list of steps to help you create consistent expressive X artwork.</p>
+              </div>
+              <div class="img img-w-caption">
+                <img src="<?php print $dir; ?>images/design-building-4-01.jpg" alt="" />
+                <p class="caption"><strong>1. Select your subject</strong><br />In this example we’ll take a look at some artwork from Justice League featuring Gal Gadot.</p>
+              </div>
+              <div class="img img-w-caption">
+                <img src="<?php print $dir; ?>images/design-building-4-02.jpg" alt="" />
+                <p class="caption"><strong>2. Isolate the subject</strong><br />Cut out the foreground subject.</p>
+              </div>
+              <div class="img img-w-caption">
+                <img src="<?php print $dir; ?>images/design-building-4-03.jpg" alt="" />
+                <p class="caption"><strong>3. Choose placement</strong><br />In this case, the subject’s position is mainly centered, so she will look best emerging from the bottom corner.</p>
+              </div>
+              <div class="img img-w-caption">
+                <img src="<?php print $dir; ?>images/design-building-4-04.jpg" alt="" />
+                <p class="caption"><strong>4. Evaluate color tone</strong><br />This poster art is a little more complex. It has a lot of existing artifacts in the background. If this is the case, a simpler background can be created from the tones in the image.</p>
+              </div>
+              <div class="img img-w-caption">
+                <img src="<?php print $dir; ?>images/design-building-4-05.jpg" alt="" />
+                <p class="caption"><strong>5. Create a background</strong><br />This background was inspired by the sky and earth tones in the artwork. We now have a simple background to place our art on.</p>
+              </div>
+              <div class="img img-w-caption">
+                <img src="<?php print $dir; ?>images/design-building-4-06.jpg" alt="" />
+                <p class="caption"><strong>6. Combine elements</strong><br />Note that the X is not overpowering. At this point, subtle design flourishes may be added, like the flare on her belt.</p>
+              </div>
+              <div class="img img-w-caption">
+                <img src="<?php print $dir; ?>images/design-building-4-07.jpg" alt="" />
+                <p class="caption"><strong>7. Crop & add branding</strong><br />The final step is to ensure that the crop is right, and to add any type or branding around the art.</p>
               </div>
             </div>
           </section>

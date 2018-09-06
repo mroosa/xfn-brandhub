9f21704866e00206a05ed7f76ffc437e7de2637d
Matthew Roosa <mroosa@agency-m.com>
2018-09-06 16:37:16 -0400

    update files

--- _templates/_structure/html-foot.php
+++ _templates/_structure/html-foot.php
@@ -1,7 +1,7 @@
     <footer>
       <div class="inner">
         <div class="logo">
-          <img src="<?php print $dir; ?>gfx/xfn-logo.png" alt="Xfinity" />
+          <a href="<?php print $dir; ?>index"><img src="<?php print $dir; ?>gfx/xfn-logo.png" alt="Xfinity" /></a>
         </div>
         <div class="info">
           <p>Copyright 2018  |  All materials subject to <a href="#">Terms and Conditions</a>  |   Questions? Contact someone@xfinity.com</p>

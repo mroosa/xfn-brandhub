c1891fc3145f41194f3b043cecddbf36a9147e3a
Matthew Roosa <mroosa@agency-m.com>
2018-09-27 13:28:46 -0400

    more sub section ghomepage randomizer

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
   // Homepage, no variables
+  $imgAry = ['homepage_lrg_1.jpg','homepage_lrg_2.jpg','homepage_lrg_3.jpg','homepage_lrg_4.jpg','homepage_lrg_5.jpg'];
+  $theImg = $imgAry[rand(0, count($imgAry)-1)];
+  $imgAry = ['homepage_mid_1.jpg','homepage_mid_2.jpg','homepage_mid_3.jpg','homepage_mid_4.jpg','homepage_mid_5.jpg'];
+  $theImg2 = $imgAry[rand(0, count($imgAry)-1)];
+  $imgAry = ['homepage_sml_1.jpg','homepage_sml_2.jpg','homepage_sml_3.jpg','homepage_sml_4.jpg','homepage_sml_5.jpg'];
+  $theImg3 = $imgAry[rand(0, count($imgAry)-1)];
 ?>
 <?php include_once($dir . "_templates/_structure/html-head.php"); ?>
 <body class="one-col home">
@@ -10,9 +16,9 @@
         <div class="content">
           <section class="section header">
             <a href="brand">
-              <img class="lrg" src="images/homepage_lrg.jpg" alt="Welcome to the Xfinity Brand Site - Let's get started" />
-              <img class="mid" src="images/homepage_mid.jpg" alt="Welcome to the Xfinity Brand Site - Let's get started" />
-              <img class="sml" src="images/homepage_sml.jpg" alt="Welcome to the Xfinity Brand Site - Let's get started" />
+              <img class="lrg" src="images/<?php print $theImg; ?>" alt="Welcome to the Xfinity Brand Site - Let's get started" />
+              <img class="mid" src="images/<?php print $theImg2; ?>" alt="Welcome to the Xfinity Brand Site - Let's get started" />
+              <img class="sml" src="images/<?php print $theImg3; ?>" alt="Welcome to the Xfinity Brand Site - Let's get started" />
             </a>
           </section>
 

43e747c0df3ebd903a4cc3b1a5cc2cfd44d5deaa
Matthew Roosa <mroosa@agency-m.com>
2018-09-27 17:33:16 -0400

    updated secondary graphics, hi-res

--- design/secondary-graphics.php
+++ design/secondary-graphics.php
@@ -24,68 +24,98 @@
 
             <h3>Xfinity Customer Commitment</h3>
             <p>Our Customer Commitment seal represents our commitment  to making our customer experience our best product, and should be used on all tactics that reference our customer guarantee or a customer experience innovation related to a key pillar.</p>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1.png" alt="" />
+            <div class="flex-col flex-3col">
+              <div class="img">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-1.png" alt="" />
+              </div>
+              <div class="spacer"></div>
+              <div class="spacer"></div>
             </div>
             <a class="button" href="#">Download</a>
 
             <h3>Speed Test Flag</h3>
             <p>Xfinity is the best internet in America, according to speedtest.net. Use this flag when using that claim if design and layout allow.</p>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1-3.png" alt="" />
+            <div class="flex-col flex-3col">
+              <div class="img">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-2.png" alt="" />
+              </div>
+              <div class="spacer"></div>
+              <div class="spacer"></div>
             </div>
             <a class="button" href="#">Download</a>
 
             <h3>X1 Channels and Experiences</h3>
             <p>Special design treatments have been developed for the Xfinity X1 interface. They should only be used within the X1 experience, and never in marketing collateral or communications.</p>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-3.png" alt="" />
-            </div>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-4.png" alt="" />
-            </div>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-5.png" alt="" />
+            <div class="flex-col flex-3col">
+              <div class="img">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-3.png" alt="" />
+              </div>
+              <div class="spacer"></div>
+              <div class="spacer"></div>
+              <div class="img">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-4.png" alt="" />
+              </div>
+              <div class="spacer"></div>
+              <div class="spacer"></div>
+              <div class="img">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-5.png" alt="" />
+              </div>
+              <div class="spacer"></div>
+              <div class="spacer"></div>
             </div>
             <a class="button" href="#">Download</a>
 
             <h3>Xfinity on Campus</h3>
             <p>Xfinity on Campus makes our products and services available to colleges in our region, helping win the loyalty of a new generation of customers. Use this only on program-related communications.</p>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1-8.png" alt="" />
+            <div class="flex-col flex-3col">
+              <div class="img double">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-6.png" alt="" />
+              </div>
+              <div class="spacer"></div>
             </div>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1-9.png" alt="" />
+            <a class="button" href="#">Download</a>
+
+            <h3>Xfinity Communities</h3>
+            <p>Xfinity Communities is our Multi Dwelling Unit (MDU) segment of Xfinity that focuses on individual properties such as apartments, condos, healthcare facilities and more. Please only use this logo in those communications.</p>
+            <div class="flex-col flex-3col">
+              <div class="img double">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-10.png" alt="" />
+              </div>
+              <div class="spacer"></div>
             </div>
             <a class="button" href="#">Download</a>
 
             <h3>Authorized Retailer and Authorized Dealer</h3>
             <p>The Xfinity Authorized Retailer and Dealer logos are for use expressly by Xfinity third-party agents and eTail environments. These logos should only be used in third-party communications by Authorized Representatives and Dealers.</p>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1-10.png" alt="" />
-            </div>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1-12.png" alt="" />
+            <div class="flex-col flex-3col">
+              <div class="img double">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-7.png" alt="" />
+              </div>
+              <div class="spacer"></div>
+              <div class="img double">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-8.png" alt="" />
+              </div>
+              <div class="spacer"></div>
             </div>
             <a class="button" href="#">Download</a>
 
             <h3>Xfinity on the Fly</h3>
             <p>Xfinity on the Fly is our in-airport lounge-like experience offering customers fast WiFi connections  while on the go. This mark should only be used within Xfinity on the Fly destinations.</p>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1-14.png" alt="" />
-            </div>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1-15.png" alt="" />
+            <div class="flex-col flex-3col">
+              <div class="img double">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-9.png" alt="" />
+              </div>
+              <div class="spacer"></div>
             </div>
             <a class="button" href="#">Download</a>
 
             <h3>Xfinity Thanks You</h3>
             <p>Xfinity Thanks You is to be used in select surprise-and-delight loyalty communications to foster commitment, cultivate relationships and build value with our customers.</p>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1-16.png" alt="" />
-            </div>
-            <div class="img">
-              <img src="<?php print $dir; ?>images/design-secondary-graphics-1-17.png" alt="" />
+            <div class="flex-col flex-3col">
+              <div class="img double">
+                <img src="<?php print $dir; ?>images/design-secondary-graphics-11.png" alt="" />
+              </div>
+              <div class="spacer"></div>
             </div>
             <a class="button" href="#">Download</a>
           </section>

11f6f78fb52ddbfc6540e99015379c46be5b4c12
Matthew Roosa <mroosa@agency-m.com>
2018-10-01 10:19:18 -0400

    inspiration sorting

--- inspiration.php
+++ inspiration.php
@@ -14,53 +14,53 @@ $activeTop = 2;
           <section class="section form">
             <div class="selection">
               <label for="product">Product</label>
-                <select id="product" name="product">
-                  <option value="">All Products</option>
-                  <option value="gig">Gig</option>
-                  <option value="double">Double Play</option>
-                  <option value="account">My Account</option>
-                  <option value="apps">Apps</option>
-                  <option value="wifi">WiFi</option>
-                  <option value="stream">Stream</option>
-                  <option value="x1">X1</option>
-                  <option value="home">Xfinity Home</option>
-                  <option value="internet">Xfinity Internet</option>
-                </select>
+              <select id="product" name="product">
+                <option value="all">All Products</option>
+                <option value="gig">Gig</option>
+                <option value="double">Double Play</option>
+                <option value="account">My Account</option>
+                <option value="apps">Apps</option>
+                <option value="wifi">WiFi</option>
+                <option value="stream">Stream</option>
+                <option value="x1">X1</option>
+                <option value="home">Xfinity Home</option>
+                <option value="internet">Xfinity Internet</option>
+              </select>
             </div>
             <div class="selection">
               <label for="tactic">Tactic</label>
-                <select id="tactic" name="tactic">
-                  <option value="">All Tactics</option>
-                  <option value="brochure">Brochure</option>
-                  <option value="collateral">Collateral</option>
-                  <option value="digital">Digital Banners</option>
-                  <option value="dm">Direct Mail</option>
-                  <option value="email">Email</option>
-                  <option value="video">Online Video</option>
-                  <option value="ooh">Out-of-Home</option>
-                  <option value="print">Print</option>
-                  <option value="social">Social</option>
-                  <option value="tv">TV</option>
-                </select>
+              <select id="tactic" name="tactic">
+                <option value="all">All Tactics</option>
+                <option value="brochure">Brochure</option>
+                <option value="collateral">Collateral</option>
+                <option value="digital">Digital Banners</option>
+                <option value="dm">Direct Mail</option>
+                <option value="email">Email</option>
+                <option value="video">Online Video</option>
+                <option value="ooh">Out-of-Home</option>
+                <option value="print">Print</option>
+                <option value="social">Social</option>
+                <option value="tv">TV</option>
+              </select>
             </div>
             <div class="selection">
               <label for="channel">Channel</label>
-                <select id="channel" name="channel">
-                  <option value="">All Channels</option>
-                  <option value="acquisition">Acquisition</option>
-                  <option value="brand">Brand</option>
-                  <option value="engagement">Customer Engagement</option>
-                  <option value="multicultural">Multiculural</option>
-                </select>
+              <select id="channel" name="channel">
+                <option value="all">All Channels</option>
+                <option value="acquisition">Acquisition</option>
+                <option value="brand">Brand</option>
+                <option value="engagement">Customer Engagement</option>
+                <option value="multicultural">Multiculural</option>
+              </select>
             </div>
             <div class="selection">
               <label for="campaign">Campaigns &amp; Programs</label>
-                <select id="campaign" name="campaign">
-                  <option value="">All </option>
-                  <option value="olympics">Olympics</option>
-                  <option value="sports">Sports</option>
-                  <option value="world-cup">World Cup</option>
-                </select>
+              <select id="campaign" name="campaign">
+                <option value="all">All </option>
+                <option value="olympics">Olympics</option>
+                <option value="sports">Sports</option>
+                <option value="world-cup">World Cup</option>
+              </select>
             </div>
           </section>
           <section class="section inspiration-gallery">
@@ -122,10 +122,4 @@ $activeTop = 2;
         </div><!-- /.content -->
       </div><!-- /.inner -->
     </main>
-    <script>
-      var $grid = $('.grid').masonry();
-      $grid.imagesLoaded().progress(function() {
-        $grid.masonry('layout');
-      });
-    </script>
 <?php include_once($dir . "_templates/_structure/html-foot.php"); ?>
--- js/inspiration.js
+++ js/inspiration.js
@@ -1,47 +1,67 @@
 $(document).ready(function() {
+  // Initiate masonry
+  var $grid = $('.grid').masonry();
+  $grid.imagesLoaded().progress(function() {
+    $grid.masonry('layout');
+  });
+
   // Create empty Tag List object
-  var TagList = {};
+  var TagList = {product:"all", tactic:"all", channel:"all", campaign:"all"};
+
 
   // Clear out the Tag List
   function clearTags() {
-    TagList = {product:"", tactic:"", channel:"", campaign:""};
+    TagList = {product:"all", tactic:"all", channel:"all", campaign:"all"};
     $(".selection").find("select").val("");
     $(".inspiration-gallery").removeClass("modified");
   }
+
   $(".selection").find("select").change(function() {
     var newTag = $(this).val();
     var thisCat = $(this).attr("id");
     TagList[thisCat] = newTag;
-    // console.log(TagList);
+    var listCount = 0;
+    $(".inspiration-gallery").removeClass("modified");
+    for (var prop in TagList) {
+      if (TagList[prop] == "all") {
+        listCount++;
+      }
+    }
+    // Add class to allow show/hide of items if "all" is not selected for all dropdowns
+    if (listCount<4) {
+      $(".inspiration-gallery").addClass("modified");
+    }
+
     $(".grid-item").each(function() {
+      var propCount = 0;
       if ($(this).attr("data-tags")) {
-        var itemTags = $(this).attr("data-tags").split(",");
-        var showThis = false;
+        var itemTags = $(this).attr("data-tags").split(", ");
+        var showThis = true;
         $(this).removeClass("show");
-        for (var i=0; i<itemTags.length; i++) {
-          if (TagList.product == itemTags[i]) {
-            showThis = true;
-          }
-          if (TagList.tactic == itemTags[i]) {
-            showThis = true;
-          }
-          if (TagList.channel == itemTags[i]) {
-            showThis = true;
-          }
-          if (TagList.campaign == itemTags[i]) {
-            showThis = true;
+
+        for (var prop in TagList) {
+          // get current prop
+          var thisProp = TagList[prop];
+          // Check to see if empty
+          if (thisProp != "all") {
+            // Loop through tags
+            itemTags.forEach(function(el) {
+              if (thisProp == el) {
+                propCount++;
+              }
+            });
+          } else {
+            propCount++
+            // validate this prop
           }
         }
-        if (showThis) {
+        // If all propr validate
+        if (propCount==4) {
           $(this).addClass("show");
         }
       }
     });
-    $(".inspiration-gallery").addClass("modified");
-    var $grid = $('.grid').masonry();
-    $grid.imagesLoaded().progress(function() {
-      $grid.masonry('layout');
-    });
+    $grid.masonry('layout');
 
   });
   $(".grid-item").hover(function() {

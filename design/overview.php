644851808b646bf3408ed806e1d3714e18cf1fad
Matthew Roosa <mroosa@agency-m.com>
2020-01-10 15:01:44 -0500

    intro text

--- design/overview.php
+++ design/overview.php
@@ -1,3 +1,6 @@
-<div class="col-1" style="display: flex; height: 100%;align-items: center; justify-content: center;">
-  <h1>Introduction</h1>
+<div class="col-1">
+  <h1>Welcome to the new era of Xfinity.</h1>
+  <p>Our guidelines outline how we can express ourselves consistently and make an impact on our customers. It's all the tools you need to bring our brand to life.</p>
+  <h2>Let's build something beautiful together.</h2>
+  <a href="#logo">Get Started</a>
 </div>

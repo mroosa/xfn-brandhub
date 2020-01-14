14473384ea75eeb938c1c2d5a06dcacf29f6e6bd
Matthew Roosa <mroosa@agency-m.com>
2020-01-14 16:19:26 -0500

    Add design system videos

--- design/design-system.php
+++ design/design-system.php
@@ -9,44 +9,13 @@
   <div class="tabs">
 
     <!-- tab -->
-    <div class="tab" id="design-system-grid" data-ttl="Visual Layout">
+    <div class="tab" id="design-system-grid">
       <div class="tab-inner">
-        <h3>Visual Layout</h3>
-        <p>&nbsp;</p>
-        <video nocontrols autoplay muted>
-          <source src="video/000.mp4" type="video/mp4">
-        </video>
-        <!-- <ul class="optionWrap">
-          <li class="img width-100"><img src="images/design-system/000.png" alt=""></li>
-        </ul> -->
-      </div>
-    </div>
-
-    <!-- tab -->
-    <div class="tab" id="design-system-horizontal" data-ttl="Dynamic Angle">
-      <div class="tab-inner">
-        <h3>Dynamic Angle</h3>
-        <p>Our dynamic angle is a tool that can be used to help organize information and create containers for imagery or copy. Dynamic angles should be used in a purposeful manner to create depth or act as a plane for devices to lay on. Please only use one angle per communication.</p>
-        <ul class="optionWrap auto" data-speed="1.5">
-          <li><img class="rounded border" src="images/design-system/02-01.jpg" alt=""></li>
-          <li class="img width-70"><img class="rounded border" src="images/design-system/02-02.jpg" alt=""></li>
-          <li><img class="rounded border" src="images/design-system/02-03.jpg" alt=""></li>
-          <li class="img width-45"><img class="rounded border" src="images/design-system/03-01.jpg" alt=""></li>
-          <li class="img width-45"><img class="rounded border" src="images/design-system/03-02.jpg" alt=""></li>
-          <li><img class="rounded border" src="images/design-system/03-03.jpg" alt=""></li>
-        </ul>
-      </div>
-    </div>
-
-    <!-- tab -->
-    <div class="tab" id="design-system-vertical" data-ttl="Hierarchy">
-      <div class="tab-inner">
-        <h3>Hierarchy</h3>
-        <p>For maximized impact, it is recommended that a hierarchy of information is used to make your communication clear. Please prioritize the headline, price point (when applicable), call-to-action or button, and logo.</p>
         <ul class="optionWrap">
-          <li class="img width-65"><img class="rounded" src="images/design-system/01-01.png" alt=""></li>
-          <li class="img width-65"><img class="rounded border" src="images/design-system/01-02.png" alt=""></li>
-          <li class="img width-40"><img class="rounded" src="images/design-system/01-03.png" alt=""></li>
+          <li><video nocontrols autoplay muted><source src="video/ds-01.mp4" type="video/mp4"></video></li>
+          <li><video nocontrols autoplay muted><source src="video/ds-02.mp4" type="video/mp4"></video></li>
+          <li><video nocontrols autoplay muted><source src="video/ds-03.mp4" type="video/mp4"></video></li>
+          <li><video nocontrols autoplay muted><source src="video/ds-04.mp4" type="video/mp4"></video></li>
         </ul>
       </div>
     </div>

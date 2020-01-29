0872affaebca47eaa43ab00efeb468a182e080b3
Matthew Roosa <mroosa@agency-m.com>
2020-01-29 15:42:37 -0500

    resources links

--- design/contact.php
+++ design/contact.php
@@ -3,11 +3,11 @@
           <p>Thanks for stopping by! Questions, comments or feedback? Feel free to email the Xfinity Brand Team. We’d love to hear from you.</p>
           <ul>
             <li><a href="#" target="_blank">Digital Asset Manager</a></li>
-            <li><a href="#" target="_blank">Xfinity Customer Comms dashboard</a></li>
-            <li><a href="#" target="_blank">Xfinity Direct Marketing dashboard</a></li>
-            <li><a href="#" target="_blank">Comcast Guidelines and Assets</a></li>
-            <li><a href="#" target="_blank">Comcast Business guidelines</a></li>
-            <li><a href="#" target="_blank">NBCU logo repository</a></li>
+            <li><a href="https://customercomms.xfinity.com/" target="_blank">Xfinity Customer Comms dashboard</a></li>
+            <li><a href="https://dashboard.xfinity.com/" target="_blank">Xfinity Direct Marketing dashboard</a> Password: directmktg1701</li>
+            <li><a href="http://xfinity.com/" target="_blank">Comcast Guidelines and Assets</a></li>
+            <li><a href="http://cbbrandguidelines.com/" target="_blank">Comcast Business guidelines</a></li>
+            <li><a href="https://logos.nbcuni.com/priorlogin" target="_blank">NBCU logo repository</a></li>
           </ul>
 
         </div><!-- /.col-full -->

3fbd61466d3b7c69559d7c02ddd8d3bd7d897d29
Matthew Roosa <mroosa@agency-m.com>
2020-02-04 14:57:31 -0500

    re-add old Sub Brands layout

--- design/sub-brands.php
+++ design/sub-brands.php
@@ -12,19 +12,57 @@
 
         <div class="sub-brand-logos">
           <div class="sub-brand-logo-1">
+            <img src="images/xfinity-logo.svg" alt="">
+          </div>
+          <div class="sub-brand-logo-2">
             <ul>
-              <li><img src="images/sub-brands/xfinity-flex.png" alt="Xfinity Flex"></li>
-              <li><img src="images/sub-brands/xfinity-stream.png" alt="Xfinity Stream"></li>
-              <li><img src="images/sub-brands/xfinity-xfi.png" alt="Xfinity xFi"></li>
-              <li><img src="images/sub-brands/xfinity-mobile.png" alt="Xfinity Mobile"></li>
-              <li><img src="images/sub-brands/xfinity-x1.png" alt="Xfinity x1"></li>
-              <li><img src="images/sub-brands/xfinity-home.png" alt="Xfinity Home"></li>
+              <li data-brand="0" class="active"><img src="images/sub-brands/xfinity-x1.svg" alt=""></li>
+              <li data-brand="1"><img src="images/sub-brands/xfinity-xfi.svg" alt=""></li>
+              <li data-brand="2"><img src="images/sub-brands/xfinity-stream.svg" alt=""></li>
+              <li data-brand="3"><img src="images/sub-brands/xfinity-flex.svg" alt=""></li>
+              <li data-brand="4"><img src="images/sub-brands/xfinity-home.svg" alt=""></li>
+              <li data-brand="5"><img src="images/sub-brands/xfinity-mobile.svg" alt=""></li>
             </ul>
-            <!-- <video autoplay muted loop nocontrols>
-              <source src="video/sub-brand-loop.mp4" type="video/mp4">
-            </video> -->
           </div>
         </div><!-- /.brand-logos -->
+        <div class="sub-brands">
+          <ul>
+            <li data-brand="0">
+              <div><img src="images/sub-brands/ex-x1.png" alt=""></div>
+              <!-- <div><p>Change the way you experience TV with the simplest, fastest and most complete way to access all your entertainment. Say it and see it with the X1 Voice Remote, browse personalized recommendations and discover a DVR that does it all.</p></div> -->
+            </li>
+            <li data-brand="1">
+              <div><img src="images/sub-brands/ex-xfi.png" alt=""></div>
+              <!-- <div><p>Personalize your in-home WIFi network, pause WiFi to connected devices and see when new devices join your network. It’s the speed, coverage and control you need for the ultimate in-home WiFi experience.</p></div> -->
+            </li>
+            <li data-brand="2">
+              <div><img src="images/sub-brands/ex-stream.png" alt=""></div>
+              <!-- <div><p>Watch your entire TV line-up at home and top networks and live sports on the go. Enjoy the freedom to stream thousands of Xfinity On Demand titles and your DVR library on any device.</p></div> -->
+            </li>
+            <li data-brand="3">
+              <div><img src="images/sub-brands/ex-flex.png" alt=""></div>
+              <!-- <div><p>Flex brings together popular streaming apps and networks including HBO®, Netflix and Prime Video in one personalized dashboard. Plus, it’s all searchable with the X1 Voice Remote.</p></div> -->
+            </li>
+            <li data-brand="4">
+              <div><img src="images/sub-brands/ex-home.png" alt=""></div>
+              <!-- <div><p>Enjoy more peace of mind with a total home security solution. Xfinity delivers the advanced technology you need to protect and manage your home with 24/7 professional monitoring.</p></div> -->
+            </li>
+            <li data-brand="5">
+              <div><img src="images/sub-brands/ex-mobile.png" alt=""></div>
+              <!-- <div><p>Discover a different kind of wireless network designed to save you money. Xfinity Mobile connects to millions of secure WiFi hotspots and the best LTE everywhere else. Design your own data plan, so you only pay for what you need.</p></div> -->
+            </li>
+          </ul>
+        </div><!-- /.sub-brands -->
+        <div class="sub-brand-nav">
+          <ul>
+            <li class="active"><a href="#" data-brand="0"><img src="images/sub-brands/xfinity-x1.svg" alt=""></a></li>
+            <li><a href="#" data-brand="1"><img src="images/sub-brands/xfinity-xfi.svg" alt=""></a></li>
+            <li><a href="#" data-brand="2"><img src="images/sub-brands/xfinity-stream.svg" alt=""></a></li>
+            <li><a href="#" data-brand="3"><img src="images/sub-brands/xfinity-flex.svg" alt=""></a></li>
+            <li><a href="#" data-brand="4"><img src="images/sub-brands/xfinity-home.svg" alt=""></a></li>
+            <li><a href="#" data-brand="5"><img src="images/sub-brands/xfinity-mobile.svg" alt=""></a></li>
+          </ul>
+        </div>
 
       </div>
 

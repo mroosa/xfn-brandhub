beb215dd249944066985fb21478ee1542bd7df73
Matthew Roosa <mroosa@agency-m.com>
2020-03-20 14:25:16 -0400

    norobots to header

--- _templates/_structure/html-head.php
+++ _templates/_structure/html-head.php
@@ -27,6 +27,7 @@
 
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="noindex" />
   <title>Xfinity Brand Hub</title>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>

19295fa376406981dad06658fcf34c578cc50ffb
Matthew Roosa <mroosa@agency-m.com>
2020-05-21 17:52:15 -0400

    button text, italic/bold emphasis

--- design/voice.php
+++ design/voice.php
@@ -2,7 +2,7 @@
 <div class="col-1">
   <h1>Voice & Messaging</h1>
   <p>Every communication is an opportunity to shift perception, establish trust and build brand love.  The Xfinity messaging and brand voice helps establish a clear purpose and unique personality that can be recognized through the words on a page, without ever seeing an image or a logo.</p>
-  <a class="button internal-explore" href="http://xfinity.com/" target="_blank"><span>Explore Messaging Guidelines</span></a>
+  <a class="button internal-explore" href="http://xfinity.com/" target="_blank"><span>Explore Voice & Messaging Guidelines</span></a>
 </div>
 <div class="col-2">
   <div class="tabs-wrapper">
@@ -11,12 +11,12 @@
     <!-- tab -->
     <div class="tab swap" data-ttl="Our Symbol" data-bg="white" id="brand-symbol_the-xfinity-x">
       <div class="tab-inner">
-        <div class="voice-tabs">
+        <div class="voice-tabs" title="Our brand voice and messaging will help you write more creatively, effectively and consistently as a champion of the Xfinity brand.">
           <div class="vtab-row">
             <div id="vtab-1" class="vtab-col txt">
               <h2>MESSAGING</h2>
               <h3>What we say</h3>
-              <p>What do we want them to know?</p>
+              <p>What do we want them to <strong><em>know?</em></strong></p>
             </div>
             <div id="vtab-2" class="vtab-col img">
               <img src="images/voice/speech.png" alt="" />
@@ -29,7 +29,7 @@
             <div id="vtab-4" class="vtab-col txt">
               <h2>VOICE & TONE</h2>
               <h3>How we say it</h3>
-              <p>How do we want them to feel?</p>
+              <p>How do we want them to <strong><em>feel?</em></strong></p>
             </div>
           </div>
         </div>

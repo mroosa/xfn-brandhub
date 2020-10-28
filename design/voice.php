9af5a75c0e2ea225973589eabb8f5f7ed11c0a72
Matthew Roosa <mroosa@agency-m.com>
2020-10-28 15:00:03 -0400

    remove commented html

--- design/voice.php
+++ design/voice.php
@@ -11,29 +11,7 @@
     <!-- tab -->
     <div class="tab swap" data-ttl="Our Symbol" data-bg="white" id="brand-symbol_the-xfinity-x">
       <div class="tab-inner">
-        <img src="images/voice/laptop.png" title="our brand voice and messaging will help you write more creatively, effectively and consistently as a champion of the Xfinity brand" alt="">
-        <!-- <div class="voice-tabs" title="Our brand voice and messaging will help you write more creatively, effectively and consistently as a champion of the Xfinity brand.">
-          <div class="vtab-row">
-            <div id="vtab-1" class="vtab-col txt">
-              <h2>MESSAGING</h2>
-              <h3>What we say</h3>
-              <p>What do we want them to <strong><em>know?</em></strong></p>
-            </div>
-            <div id="vtab-2" class="vtab-col img">
-              <img src="images/voice/speech.png" alt="" />
-            </div>
-          </div>
-          <div class="vtab-row">
-            <div id="vtab-3" class="vtab-col img">
-              <img src="images/voice/laptop.png" alt="" />
-            </div>
-            <div id="vtab-4" class="vtab-col txt">
-              <h2>VOICE & TONE</h2>
-              <h3>How we say it</h3>
-              <p>How do we want them to <strong><em>feel?</em></strong></p>
-            </div>
-          </div>
-        </div> -->
+        <img src="images/voice/laptop.png" title="Our brand voice and messaging will help you write more creatively, effectively and consistently as a champion of the Xfinity brand." alt="">
       </div>
     </div>
 

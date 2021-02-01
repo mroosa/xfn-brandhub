9bb3918dc068c24f90880c9449384f4f29969c4c
Matthew Roosa <mroosa@agency-m.com>
2021-02-01 14:17:27 -0500

    set redirect to root-php

--- messaging-check.php
+++ messaging-check.php
@@ -8,7 +8,7 @@
 
   // Check user login or not
   if(!isset($_SESSION['uname'])){
-    header('Location: ../login?d=messaging/' . $dest);
+    header('Location: /login?d=messaging/' . $dest);
     exit;
   } else {
     if (is_file('messaging/' . $dest)){

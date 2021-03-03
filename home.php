5fe3dc62afec270cd1374a8a85bcdef592675771
Matthew Roosa <mroosa@agency-m.com>
2021-03-03 09:31:21 -0500

    make 25% smaller

--- home.php
+++ home.php
@@ -26,7 +26,7 @@
               c5.4,0,8.8-1.9,11.7-5.7l18-23.9l42.2,58.3c2.7,3.8,6.3,5.7,11.7,5.7h12.3L51.2,38.6z" />
               </g>
             </svg>
-            <br><br>
+            <br><br><br>
             <h1>Welcome to the <br class="mobile-only">Xfinity Brand Hub</h1>
             <p class="disc">This is the first of many exciting updates to our site as we transition to our new visual identity. Explore the helpful resources below, and feel free to reach out to the brand team with any questions.</p>
           </div>
@@ -35,7 +35,7 @@
 
             <div class="new-tile" id="tile-1">
               <img src="images/landing/icon-old.jpg" alt="">
-              <h2>Current Brand Guidelines</h2>
+              <h2>Current Brand<br>Guidelines and Assets</h2>
               <hr>
               <p class="tile-info" >Please refer to these guidelines for communications prior to the Olympics (100 days out)</p>
               <a class="button" href="brandhub"><span>Explore</span></a>
@@ -43,7 +43,7 @@
 
             <div class="new-tile" id="tile-2">
               <img src="images/landing/icon-olympics.jpg" alt="">
-              <h2>Olympics Brand Guidelines and Assets</h2>
+              <h2>Olympics Brand<br>Guidelines and Assets</h2>
               <hr>
               <p class="tile-info" >Guidelines, assets and resources for all Olympics-related communications</p>
               <a class="button" href="http://xfinity.com/" target="_blank"><span>Explore</span></a>
@@ -53,7 +53,7 @@
               <img src="images/landing/icon-new.jpg" alt="">
               <h2>New Brand<br>Style Guide</h2>
               <hr>
-              <p class="tile-info" >Interim style guide including high-level guidance and asset downloads. Full Brand Guide will be available in early Q2.</p>
+              <p class="tile-info" >Interim style guide including high-level guidance and asset downloads.<br> Full Brand Guide will be available in early Q2.</p>
               <a class="button" href="files/09945_New VisID One Sheet_R6.pdf" target="_blank"><span>Download</span></a>
             </div>
 

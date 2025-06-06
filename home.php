f23ac63c7ccc0ef7551d269ffdc3c656107c302a
Matthew Roosa <nasman98@yahoo.com>
2025-06-06 15:28:06 -0400

    remove user login check for archive

--- home.php
+++ home.php
@@ -1,11 +1,11 @@
 <?php
-  include_once("_templates/_db/config.php");
+  // include_once("_templates/_db/config.php");
 
-  // Check user login or not
-  if(!isset($_SESSION['uname'])){
-    header('Location: login');
-    exit;
-  }
+  // // Check user login or not
+  // if(!isset($_SESSION['uname'])){
+  //   header('Location: login');
+  //   exit;
+  // }
 
   $dir = "";
   $scriptList = [];
